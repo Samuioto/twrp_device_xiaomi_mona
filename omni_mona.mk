3bf1169081f53224bfa4e77f153ea220b10aaf06
Samuioto <90185210+Samuioto@users.noreply.github.com>
2021-11-08 14:58:31 +0800

    remove omni staff

--- omni_mona.mk
+++ omni_mona.mk
@@ -13,10 +13,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from mona device
 $(call inherit-product, device/xiaomi/mona/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mona
 PRODUCT_NAME := omni_mona
