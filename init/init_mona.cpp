f61d75cc1c089bac759d4395e0e5cf304c3384c9
Samuioto <90185210+Samuioto@users.noreply.github.com>
2021-11-12 19:14:51 +0800

    Update init_mona.cpp

--- init/init_mona.cpp
+++ init/init_mona.cpp
@@ -76,7 +76,7 @@ void vendor_load_properties() {
     property_override("ro.vendor.build.security_patch", "2099-12-31");
     property_override("ro.bootimage.build.date.utc", "1546335651");
     property_override("ro.build.date.utc", "1546335651");
-#ifndef BUILD_FOR_CMI
+#ifndef BUILD_FOR_MONA
     const std::string device_hwversion = GetProperty("ro.boot.hwversion", "");
     if (device_hwversion.at(0) == '1') {
         model_property_override("mona", "xiaomi civi");
